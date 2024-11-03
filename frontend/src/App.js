67a35d2e0bf35fd6c10f97a9386d828a3df4ed78
Mohamad Nehme <moudynehme1999@gmail.com>
2024-11-03 14:24:08 +0200

    Starting The Front End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
+import { Outlet } from "react-router-dom";
+
 const App = () => {
   return (
-    <div>
-      App
-    </div>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
