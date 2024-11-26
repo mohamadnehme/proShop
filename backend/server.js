bc58da4d2210d2fc7e7dd11fab8a55abed85da63
Mohamad Nehme <moudynehme1999@gmail.com>
2024-11-26 14:20:29 +0200

    Backend Authentication

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorHandler.js';
+import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
